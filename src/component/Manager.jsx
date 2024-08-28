deab87cc415459ee8fe82d07ad785e8115fc5be2
Tarun910 <namdevtarun910@gmail.com>
2024-08-28 22:45:46 +0530

    Fixed UI

--- src/component/Manager.jsx
+++ src/component/Manager.jsx
@@ -120,7 +120,9 @@ const Manager = () => {
         <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
       </div>
 
-      <div className="mycontainer">
+      <div className=" p-2 md:p-14 md:mycontainer">
+        {/* <div className="p-2 md:p-0 md:mycontainer"> */}
+        {/* <div className="mycontainer"> */}
         <h1 className="text-4xl font-bold text-center">
           <span className="text-green-500">&lt;</span>
           Pass
@@ -139,7 +141,9 @@ const Manager = () => {
             name="site"
             id="site"
           />
-          <div className="flex w-full justify-between gap-8">
+          <div className="flex flex-col md:flex-row w-full justify-between gap-8">
+            {/* <div className="flex w-full justify-between gap-8"> */}
+            {/* <div className="flex w-full justify-between gap-8"> */}
             <input
               onChange={handleChange}
               value={form.username}
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="bg-black text-white flex justify-around items-center px-4 py-2">
+    <nav className="bg-black text-white flex justify-around items-center px-4 py-4">
       <div className="logo font-bold">
         <span className="text-green-500">&lt;</span>
         Pass
